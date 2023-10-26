2d286a6858a1ef04ab2eb009798b2cd149cbd3ce
Roman29 <roman.mazur29@gmail.com>
2023-10-27 00:46:38 +0300

    ubuntu

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion" {
-  ami           = "ami-0fb820135757d28fd"
+  ami           = "ami-0ab1a82de7ca5889c"
   instance_type = "t2.micro"
   key_name      = "dev_ops_itca"
   subnet_id     = aws_subnet.public.id
@@ -21,25 +21,25 @@ resource "aws_instance" "bastion" {
 }
 
 resource "aws_instance" "ci_cd" {
-  ami           = "ami-0fb820135757d28fd"
+  ami           = "ami-0ab1a82de7ca5889c"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_1.id
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-0fb820135757d28fd"
+  ami           = "ami-0ab1a82de7ca5889c"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private_1.id
 }
 
 resource "aws_instance" "backend" {
-  ami           = "ami-0fb820135757d28fd"
+  ami           = "ami-0ab1a82de7ca5889c"
   instance_type = "t3.medium"
   subnet_id     = aws_subnet.private_1.id
 }
 
 resource "aws_instance" "monitoring" {
-  ami           = "ami-0fb820135757d28fd"
+  ami           = "ami-0ab1a82de7ca5889c"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_1.id
 }
